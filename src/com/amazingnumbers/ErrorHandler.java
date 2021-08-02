c082ba59223f35792fa0a68779e2fe8edd34ba15
bogdanm01 <bogi.milojevic@gmail.com>
2021-08-02 19:50:42 +0200

    'implemented support for multiple search properties'

--- src/com/amazingnumbers/ErrorHandler.java
+++ src/com/amazingnumbers/ErrorHandler.java
@@ -1,21 +1,14 @@
 package com.amazingnumbers;
 
+import java.util.Arrays;
+
 /**
-** Methods return true if there are any errors
+ ** Methods return true if there are any errors
  */
 
 public final class ErrorHandler {
 
-    static final String VALID_PROPERTIES = "even odd buzz duck spy palindromic gapful square sunny";
-
-    public static boolean checkOnePropertyError(String firstSearchParameter) { // return true if there any errors
-        if (!isParameterValid(firstSearchParameter)) {
-            System.out.println("The property [" + firstSearchParameter.toUpperCase() + "] is wrong.");
-            System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY]");
-            return true;
-        }
-        return false;
-    }
+    static final String VALID_PROPERTIES = "even odd buzz duck spy palindromic gapful square sunny jumping";
 
     public static boolean checkNumbersErrors(long number1, long number2) { // return true if there are any errors
         boolean flag = false;
@@ -30,37 +23,61 @@ public final class ErrorHandler {
         return flag;
     }
 
-    public static boolean checkTwoPropertiesErrors(String firstSearchParameter, String secondSearchParameter) {
-        if (!(isParameterValid(firstSearchParameter)) && (isParameterValid(secondSearchParameter))) {
-            System.out.println("The property [" + firstSearchParameter.toUpperCase() + "] is wrong.");
-            System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY]");
-            return true;
-        }
-        if (isParameterValid(firstSearchParameter) && !(isParameterValid(secondSearchParameter))) {
-            System.out.println("The property [" + secondSearchParameter.toUpperCase() + "] is wrong.");
-            System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY]");
-            return true;
+    public static boolean checkProperties(String[] searchProperties) {
+        String[] invalidProperties = new String[searchProperties.length];
+        int numberOfInvalidProperties = getNumberOfInvalidProperties(searchProperties, invalidProperties);
 
-        }
-        if (!(isParameterValid(secondSearchParameter)) && !(isParameterValid(firstSearchParameter))) {
-            System.out.println("The properties [" + firstSearchParameter.toUpperCase()+", "+ secondSearchParameter.toUpperCase() + "] are wrong.");
-            System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY]");
+        if (numberOfInvalidProperties == 1) {
+            System.out.println("The property [" + invalidProperties[0] + "] is wrong.");
+            printAvailableProperties();
+            return true;
+        } else if (numberOfInvalidProperties > 1) {
+            System.out.println("The properties " + Arrays.toString(invalidProperties) + " are wrong.");
+            printAvailableProperties();
             return true;
         }
-        if (areMutuallyExclusive(firstSearchParameter, secondSearchParameter)) { // if parameters cancel each out
-            System.out.println("The request contains mutually exclusive properties: [" + firstSearchParameter.toUpperCase() + ", " + secondSearchParameter.toUpperCase() + "]");
-            System.out.println("There are no numbers with these properties.");
-            return true;
+
+        if (searchProperties.length > 1) {
+            return areMutuallyExclusive(searchProperties);
         }
         return false;
     }
 
-    private static boolean areMutuallyExclusive (String firstParameter, String SecondParameter) {
-        if ((firstParameter.equalsIgnoreCase("EVEN") && SecondParameter.equalsIgnoreCase("ODD")) || (firstParameter.equalsIgnoreCase("ODD") && SecondParameter.equalsIgnoreCase("EVEN")))
-            return true;
-        if ((firstParameter.equalsIgnoreCase("DUCK") && SecondParameter.equalsIgnoreCase("SPY")) || (firstParameter.equalsIgnoreCase("SPY") && SecondParameter.equalsIgnoreCase("DUCK")))
-            return true;
-        return (firstParameter.equalsIgnoreCase("SUNNY") && SecondParameter.equalsIgnoreCase("SQUARE")) || (firstParameter.equalsIgnoreCase("SQUARE") && SecondParameter.equalsIgnoreCase("SUNNY"));
+    private static int getNumberOfInvalidProperties(String[] searchProperties, String[] invalidProperties) {
+        int numberOfInvalidProperties = 0;
+        for (String searchProperty : searchProperties) {
+            if (!(isParameterValid(searchProperty))) {
+                invalidProperties[numberOfInvalidProperties] = searchProperty.toUpperCase();
+                numberOfInvalidProperties++;
+            }
+        }
+        return numberOfInvalidProperties;
+    }
+
+    private static void printAvailableProperties() {
+        System.out.println("Available properties: [BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY, JUMPING, EVEN, ODD]");
+    }
+
+    private static boolean areMutuallyExclusive (String[] searchProperties) {
+        boolean errorsExist = false;
+        String arrayToString = Arrays.toString(searchProperties);
+
+        if ((arrayToString.contains("EVEN") && arrayToString.contains("ODD")) || (arrayToString.contains("even") && arrayToString.contains("odd"))) {
+            System.out.println("The request contains mutually exclusive properties: [EVEN, ODD]");
+            System.out.println("There are no numbers with these properties.");
+            errorsExist = true;
+        }
+        if ((arrayToString.contains("SQUARE") && arrayToString.contains("SUNNY")) || (arrayToString.contains("square") && arrayToString.contains("sunny"))) {
+            System.out.println("The request contains mutually exclusive properties: [SQUARE, SUNNY]");
+            System.out.println("There are no numbers with these properties.");
+            errorsExist = true;
+        }
+        if ((arrayToString.contains("DUCK") && arrayToString.contains("SPY")) || (arrayToString.contains("duck") && arrayToString.contains("spy"))) {
+            System.out.println("The request contains mutually exclusive properties: [DUCK, SPY]");
+            System.out.println("There are no numbers with these properties.");
+            errorsExist = true;
+        }
+        return errorsExist;
     }
 
     private static boolean isParameterValid(String searchParameter) { // checks if searched property is valid, does it exist
